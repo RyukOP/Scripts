e848cac553e633ee7cb2633df4096a36f58ac0df
RyukOP <orbisturic@gmail.com>
2014-05-22 01:58:12 -0700

    RyukViktor
    RyukViktor

--- Viktor.lua
+++ Viktor.lua
@@ -22,6 +22,7 @@ function OnLoad()
 	ts = TargetSelector(TARGET_LESS_CAST,rRng,DAMAGE_MAGIC,false)
 	ts.name = "Viktor"
 	Config:addTS(ts)
+	PrintChat("<font color='#E97FA5'>> RyukViktor!</font>")
 end
 
 function OnTick()
